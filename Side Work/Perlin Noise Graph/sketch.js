0df13ce5e7cb9c8dc987ee51789f1d4771c9a1c3
Ryan Weston <ryanweston@Ryans-MacBook-Pro-2.local>
2018-11-16 17:48:39 +0000

    Changes colour mapped with height, moves attempting to fill graph

--- Side Work/Perlin Noise Graph/sketch.js
+++ Side Work/Perlin Noise Graph/sketch.js
@@ -5,7 +5,7 @@
 //00_Template
 //The purpose of this sketch is to provide a basic template
 //for P5.js projects
-
+var start = 0;
 //Initialization function
 function setup() {
   //Print a message to the console - to view using Chrome:
@@ -17,18 +17,21 @@ function setup() {
 //Rendering function
 function draw() {
   //Set a background color
-background(255);
-noFill();
-var xVar = 0;
-var x;
-
+background(100);
+var xVar = start;
 
 beginShape();
-for (x = 0 ;x < width; x++) {
-  var y = noise(xVar) * height;
-  xVar += 0.01;
+for (var x = 0; x < width; x++) {
+  var y = map(noise(xVar),0,1,0,height);
+  var colour = map(y, 0, 800, 0, 255);
+  fill(colour);
+  strokeWeight(3);
   vertex (x, y);
+  xVar += 0.01;
 }
+
 endShape();
+start += 0.01;
+
 
 }

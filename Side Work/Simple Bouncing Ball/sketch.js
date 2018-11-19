e92e8a2d48fc64816e6c115cb470d49b29d33c4a
Ryan Weston <ryanweston@Ryans-MacBook-Pro-2.local>
2018-11-19 10:01:39 +0000

    fixed bouncy ball, added bounce on touch

--- Side Work/Simple Bouncing Ball/sketch.js
+++ Side Work/Simple Bouncing Ball/sketch.js
@@ -5,34 +5,30 @@
 //00_Template
 //The purpose of this sketch is to provide a basic template
 //for P5.js projects
-
+var x=0;
+var speed = 2;
+var radius = 50;
 //Initialization function
 function setup() {
   //Print a message to the console - to view using Chrome:
   //View > Developer > Developer Tools > Console
   console.log("Initialization: OK")
-  createCanvas(500,500);
+  createCanvas(800,800);
 }
 
 //Rendering function
 function draw() {
   //Set a background color
   background(255, 255, 0);
-
-  if (ball.x == 500 { 
-
-  }
-
-  //Print a message to the console - to view using Chrome:
-  //View > Developer > Developer Tools > Console
-  console.log("Rendering...")
-  noLoop();
-}
-
-function Ball {
-  noFill();
-  strokeWeight(3);
   stroke('#000000');
-  ellipse(150,150,200,200);
-
+  strokeWeight(3);
+  noFill();
+  ellipse(x, 400,radius*2,radius*2);
+  x += speed;
+  if (x+radius == 800) {
+    speed = -2;
+  } else if (x-radius == 0) {
+    speed = 2;
+  }
+  console.log(x);
 }

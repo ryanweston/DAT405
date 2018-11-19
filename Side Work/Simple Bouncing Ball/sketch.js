abcc996df07030006a1a662dc9e07fbac7b51e54
Ryan Weston <ryanweston@Ryans-MacBook-Pro-2.local>
2018-11-19 10:55:14 +0000

    added height to bouncy ball movement

--- Side Work/Simple Bouncing Ball/sketch.js
+++ Side Work/Simple Bouncing Ball/sketch.js
@@ -1,34 +1,35 @@
-//https://github.com/stavrosdidakis/DAT-GAD-405_2017
-//https://p5js.org
-
-//DAT405 / GAD405
-//00_Template
-//The purpose of this sketch is to provide a basic template
-//for P5.js projects
-var x=0;
-var speed = 2;
+var speedX = 5;
+var speedY = 5;
 var radius = 50;
+var x = 200;
+var y = 200;
 //Initialization function
 function setup() {
   //Print a message to the console - to view using Chrome:
   //View > Developer > Developer Tools > Console
   console.log("Initialization: OK")
-  createCanvas(800,800);
+  createCanvas(800,600);
+
 }
 
 //Rendering function
 function draw() {
   //Set a background color
   background(255, 255, 0);
+
+  x = x + speedX;
+  y = y + speedY;
+
+  if ((x+radius > width) || (x-radius < 0)) {
+    speedX = speedX * -1;
+  }
+  if ((y+radius > height) || (y-radius < 0)) {
+    speedY = speedY * -1;
+  }
+
   stroke('#000000');
   strokeWeight(3);
   noFill();
-  ellipse(x, 400,radius*2,radius*2);
-  x += speed;
-  if (x+radius == 800) {
-    speed = -2;
-  } else if (x-radius == 0) {
-    speed = 2;
-  }
+  ellipse(x, y,radius*2,radius*2);
   console.log(x);
 }

9bea1783186a294e6b5d3cfb0eefd0047a944255
Ryan Weston <ryanweston@Ryans-MacBook-Pro-2.local>
2018-11-19 10:59:21 +0000

    documented finished bouncy ball

--- Side Work/Simple Bouncing Ball/sketch.js
+++ Side Work/Simple Bouncing Ball/sketch.js
@@ -1,6 +1,9 @@
+//Sets speed for the ball, pixels travelled on each tick.
 var speedX = 5;
 var speedY = 5;
+//Sets size of the ball, half of actual circle size.
 var radius = 50;
+//Starting position of the ball. Cannot be less than circle size.
 var x = 200;
 var y = 200;
 //Initialization function
@@ -17,9 +20,12 @@ function draw() {
   //Set a background color
   background(255, 255, 0);
 
+  //Moves ball, adds distance per tick to original position of ball.
   x = x + speedX;
   y = y + speedY;
 
+  //Check Y & X position of ball and changes speed accordingly. Negative speed
+  //changes direction of the ball. Radius adds hitbox for the ball.
   if ((x+radius > width) || (x-radius < 0)) {
     speedX = speedX * -1;
   }
@@ -27,6 +33,7 @@ function draw() {
     speedY = speedY * -1;
   }
 
+  //Circle styling and draws circle. 
   stroke('#000000');
   strokeWeight(3);
   noFill();
